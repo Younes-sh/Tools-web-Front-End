4e6561d15ac935b394cf79db71bdf9fc9dc7daa5
younes-sh <sheikhlaryounes@gmail.com>
2026-03-14 14:21:34 +0100

    I created the image converter page

---
+++ app/(tools)/imageConverter/page.tsx
@@ -0,0 +1,11 @@
+import ImageConverter from '../../../components/tools/ImageConverter';
+
+export default function ImageConverterPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white pt-24 pb-16">
+      <div className="container mx-auto px-4">
+        <ImageConverter />
+      </div>
+    </div>
+  );
+}
